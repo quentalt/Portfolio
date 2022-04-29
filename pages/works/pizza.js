cfcc4ce888c339e24bc0f922f2a8362f4f622d39
quentalt <72386166+quentalt@users.noreply.github.com>
2022-04-29 20:54:27 +0200

    Update pizza.js

--- pages/works/pizza.js
+++ pages/works/pizza.js
@@ -5,13 +5,11 @@ import {
   List,
   ListItem,
   SimpleGrid,
-  UnorderedList,
   Heading,
   Center,
   Image
 } from '@chakra-ui/react'
 import Layout from '../../components/layouts/article'
-import { ExternalLinkIcon } from '@chakra-ui/icons'
 import { Title, WorkImage, Meta } from '../../components/work'
 import P from '../../components/paragraph'
 
