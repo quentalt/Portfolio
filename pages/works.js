11d8a6eddc32f155d0df5883d445c1e65b8df26a
quentalt <72386166+quentalt@users.noreply.github.com>
2022-04-29 21:11:07 +0200

    Update works.js

--- pages/works.js
+++ pages/works.js
@@ -79,29 +79,6 @@ const Works = () => (
           Vieux travaux
         </Heading>
       </Section>
-
-      {/* <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section delay={0.5}>
-          <WorkGridItem id="pichu2" thumbnail={thumb} title="Pichu*Pichu">
-            Twitter client app for iPhone Safari
-          </WorkGridItem>
-        </Section>
-        <Section delay={0.5}>
-          <WorkGridItem
-            id="freedbtagger"
-            thumbnail={thumbFreeDBTagger}
-            title="freeDBTagger"
-          >
-            Automatic audio file tagging tool using FreeDB for Windows
-          </WorkGridItem>
-        </Section>
-        {/* <Section delay={0.6}>
-          <WorkGridItem id="amembo" thumbnail={thumbAmembo} title="Amembo">
-            P2P private file sharing tool with MSN Messenger integration for
-            Windows
-          </WorkGridItem>
-        </Section> }
-      </SimpleGrid> */}
     </Container>
   </Layout>
 )
